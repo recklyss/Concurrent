73e6ff9cb9e1d7bccfa40e87757573e3b14a5513
Fatezhang <762344938@qq.com>
2019-05-26 17:55:01 +0800

    简单实现可重入锁 不使用AQS实现

--- src/main/java/com/mime/concurrent/MyLockPkg/MyLock.java
+++ src/main/java/com/mime/concurrent/MyLockPkg/MyLock.java
@@ -9,11 +9,11 @@ import java.util.concurrent.locks.Lock;
  */
 public class MyLock implements Lock {
 
-    private boolean locked = false;
+    private boolean locked = false;// 当前锁是否已经被线程使用
 
-    private int lockCount;
+    private int lockCount;// 保存当前线程加锁的次数
 
-    private Thread thread;
+    private Thread thread;// 保存当前线程是哪一个
 
     public synchronized void lock() {
         try {
@@ -23,6 +23,7 @@ public class MyLock implements Lock {
                 System.out.println("我等会儿。。。");
                 wait();
             }
+            // 如果没有线程使用锁或者获取锁的是当前线程 加锁计数器+1 然后thread指向获取锁的线程
             this.thread = Thread.currentThread();
             lockCount++;
             locked = true;
@@ -33,13 +34,19 @@ public class MyLock implements Lock {
         }
     }
 
+    /**
+     * 释放锁的操作
+     */
     public synchronized void unlock() {
         try {
+            // 如果不是当前线程 不需要做任何操作
             if(this.thread == Thread.currentThread()){
+                // 锁计数器减1 如果当前线程获取锁个数释放完成
                 lockCount--;
                 if(lockCount == 0 ){
-                    notifyAll();
+                    // 释放完成 加锁标志置为false 再唤醒等待锁的线程
                     locked = false;
+                    notifyAll();
                 }
             }
         } catch (Exception e) {
--- src/main/java/com/mime/concurrent/MyLockPkg/Sequence.java
+++ src/main/java/com/mime/concurrent/MyLockPkg/Sequence.java
@@ -1,5 +1,8 @@
 package com.mime.concurrent.MyLockPkg;
 
+/**
+ * 一个计数器
+ */
 public class Sequence {
 
     private int value;
@@ -10,6 +13,9 @@ public class Sequence {
         this.value = 0;
     }
 
+    /**
+     * 获取下一个累加器的值
+     */
     public int getNext(){
         lock.lock();
         this.value ++;
@@ -18,6 +24,9 @@ public class Sequence {
         return this.value;
     }
 
+    /**
+     * 检测是否可重入
+     */
     public void sout(){
         lock.lock();
         System.out.println(this.value);
